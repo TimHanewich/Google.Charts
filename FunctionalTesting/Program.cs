bb6bb3e981cddd563513b26d481c3b86e8dad62c
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-28 20:22:14 -0700

    tested new version (from last commit)

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -18,15 +18,16 @@ namespace FunctionalTesting
             Vals.Add(15);
             Vals.Add(60);
             
-            ChartRequest cr = new ChartRequest();
-            cr.AxisX.Minimum = 1;
-            cr.AxisX.Maximum = 5;
-            cr.ShowAxes = false;
-            cr.AxisX.Label = "Hello";
-            cr.AxisY.Label = "der";
-            cr.LoadData(Vals.ToArray());
+            LineChartRequest lcr = new LineChartRequest();
+            lcr.LoadData(Vals.ToArray());
+            lcr.ShowAxes = true;
+            lcr.AxisX.Minimum = 0;
+            lcr.AxisX.Maximum = 10;
+            lcr.AxisY.Minimum = 5;
+            lcr.AxisY.Maximum = 15;
 
-            Stream s = cr.DownloadChartAsync().Result;
+            Stream s = lcr.DownloadChartAsync().Result;
+
             Stream tocopyto = System.IO.File.OpenWrite(path);
             s.Seek(0, SeekOrigin.Begin);
             s.CopyTo(tocopyto);
